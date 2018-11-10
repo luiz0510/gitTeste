fb4ffeee3772e8dbda319b6d6fa44046bf1e498d
Luiz Eduardo <luiz.vasconcelos510@gmail.com>
2018-11-10 15:47:21 -0200

    teste de let e cosnt

---
+++ teste.js
@@ -0,0 +1,18 @@
+let inteiro = 2,
+    float = 2.3,
+    nome = 'luiz',
+    boleano = true
+
+const cheio = 2,
+    flutuante = 4.4,
+    name = 'sandy',
+    bolado = false
+
+console.log(inteiro,
+    float,
+    nome,
+    boleano,
+    cheio,
+    flutuante,
+    name,
+    bolado)             
