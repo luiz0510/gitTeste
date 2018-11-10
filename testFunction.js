9a37804bb054a53da720b2eacd6b35d87b803b1f
Luiz Eduardo <luiz.vasconcelos510@gmail.com>
2018-11-10 16:28:03 -0200

    teste de funcoes matematicas

---
+++ testFunction.js
@@ -0,0 +1,19 @@
+function somar(a, b) {
+    return a + b
+}
+
+function subtrair(a, b) {
+    return a - b
+}
+
+function dividir(a, b) {
+    return a / b
+}
+
+function multiplicar(a, b) {
+    return a * b
+}
+
+const valor1 = 4, valor2 = 8
+
+console.log(dividir(valor1, valor2), multiplicar(valor1, valor2), subtrair(valor1, valor2), somar(valor1, valor2))
